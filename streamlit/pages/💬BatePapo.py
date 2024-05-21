933381576d4029b2dc4a12ff44e5bca125c5006f
Antonio Carlos Souza de Abrantes <47427886+antonioabrantes@users.noreply.github.com>
2024-05-21 17:23:49 -0300

    Add files via upload

---
+++ streamlit/pages/💬BatePapo.py
@@ -0,0 +1,76 @@
+import streamlit as st
+
+from pathlib import Path
+import google.generativeai as genai
+from dotenv import load_dotenv
+import pandas as pd
+import numpy as np
+import os
+import time
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Importa os detalhes das vagas de emprego
+from jobs_details import jobs_details as data
+
+# Instrução do sistema para o modelo generativo
+system_instruction = f"""
+
+Seu nome é Sophia, um assistente virtual que ajuda a tirar suas dúvidas sobre o processamento de pedidos de patentes na fase recursal no INPI.  
+
+Informação sobre o processamento em formato JSON: {data}
+
+Seu trabalho é entender a pergunta do usuário por meio de perguntas, para no final indicar a resposta. 
+
+Tenha certeza de perguntar sobre as razões do indeferimento do pedido e se o usuário apresenta em sua petição de recurso um novo quadro reivindicatório. 
+
+Quando entender a pergunta do usuário, sugerir o modelo de parecer mais provável a ser usado, se este pedido deve retornar ao primeiro exame ou se deve sofrer exigência.
+
+"""
+
+# Configura a API para o modelo genai
+genai.configure(api_key=os.getenv("gemini_api_key"))
+
+
+# Inicializa o modelo generativo
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-pro-latest",
+  system_instruction=system_instruction
+)
+
+# Mensagem inicial do modelo
+initial_model_message = "Olá, eu sou Sophia, um assistente virtual que te ajuda a tirar suas dúvidas sobre o processamento de recursos de pedidos de patente. Como você se chama?"
+
+# Mensagem com as vagas disponíveis
+
+# Inicializa a conversa do assistente virtual
+if "chat_encontra" not in st.session_state:
+    st.session_state.chat_encontra = model.start_chat(history=[{'role':'model', 'parts': [initial_model_message]}])
+
+# Título da página
+st.title('BatePapo 💬')
+
+# Introdução do assistente virtual
+st.write("A Assistente Virtual Sophia está aqui para te ajudar a tirar suas dúvidas sobre o processamento de recursos de paedidos de patente! Atualmente o assistente tem informações mais comuns já cadastradas. Vamos começar?")
+
+# Exibe o histórico de conversa
+for i, message in enumerate(st.session_state.chat_encontra.history):
+  if message.role == "user":
+    with st.chat_message("user"):
+      st.markdown(message.parts[0].text)
+  else:
+    with st.chat_message("assistant"):
+      st.markdown(message.parts[0].text)
+
+# Entrada do usuário
+user_query = st.chat_input('Você pode falar ou digitar sua resposta aqui:')
+
+
+# Processamento da entrada do usuário e resposta do assistente
+if user_query is not None and user_query != '':
+    with st.chat_message("user"):
+      st.markdown(user_query)
+    with st.chat_message("assistant"):
+        ai_query = st.session_state.chat_encontra.send_message(user_query).text
+        st.markdown(ai_query)
---
+++ streamlit/pages/📊️Estatisticas.py
@@ -0,0 +1,73 @@
+import streamlit as st
+import streamlit as st
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+
+
+# Adicionando título 
+# https://emojipedia.org/search?q=spy
+st.title('Estatísticas 📊️')
+
+# Adicionando descrição do projeto
+st.write("Estatísticas de pedidos subjudice 15.23.")
+
+
+url = "http://www.cientistaspatentes.com.br/apiphp/patents/query/?q={%22mysql_query%22:%22divisao,count(*)%20FROM%20arquivados%20where%20despacho=%2715.23%27%20and%20year(data)%3E=2000%20group%20by%20divisao%20order%20by%20count(*)%20desc%22}"
+# Definindo cabeçalhos para a requisição
+headers = {
+    "Accept": "application/json",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+}
+
+
+try:
+    # Requisição para obter os dados JSON
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()  # Verificar se a requisição foi bem-sucedida
+
+    # Tentar decodificar o JSON
+    data = response.json()
+
+    # Carregar os dados JSON em um DataFrame
+    df = pd.DataFrame(data['patents'])
+    df['divisao'] = df['divisao'].fillna('Unknown')
+
+    # Verificar e converter a coluna 'count' para inteiro
+    df['count'] = pd.to_numeric(df['count'], errors='coerce')
+
+    # Mostrar o DataFrame
+    st.write("Valores", df)
+
+    # Exibir o gráfico de linhas
+    # st.line_chart(df.set_index('divisao')['count'])
+    
+    fig, ax = plt.subplots()
+    ax.plot(df['divisao'], df['count'], marker='o')
+
+    # Adicionar linhas verticais
+    for i, label in enumerate(df['divisao']):
+        ax.axvline(x=i, color='gray', linestyle='--', linewidth=0.5)
+
+    # Adicionar linhas horizontais
+    for count in df['count']:
+        ax.axhline(y=count, color='gray', linestyle='--', linewidth=0.5)
+        
+    # Adicionar rótulos e título
+    ax.set_xlabel('Divisão')
+    ax.set_ylabel('Count')
+    ax.set_title('Incidência por Divisão Técnica')
+    ax.set_xticks(range(len(df['divisao'])))
+    ax.set_xticklabels(df['divisao'], rotation=90)
+
+    # Mostrar o gráfico no Streamlit
+    st.pyplot(fig)
+
+except requests.exceptions.HTTPError as http_err:
+    st.error(f"HTTP error occurred: {http_err}")
+except requests.exceptions.RequestException as req_err:
+    st.error(f"Error occurred during request: {req_err}")
+except ValueError as json_err:
+    st.error(f"JSON decode error: {json_err}")
+except Exception as err:
+    st.error(f"An unexpected error occurred: {err}")
