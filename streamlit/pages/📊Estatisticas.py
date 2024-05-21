018399869bf428e48e41cc7c25544c16e73f7a7e
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-05-21 20:31:54 -0300

    Update 📊Estatisticas.py

--- streamlit/pages/📊Estatisticas.py
+++ streamlit/pages/📊Estatisticas.py
@@ -42,7 +42,7 @@ option2 = {
 }
 
 # Widget de seleção para escolher entre os gráficos
-chart_selection = st.radio("Selecione o gráfico:", ("Gráfico 1", "Gráfico 2", "Gráfico 3", "Gráfico 4"))
+chart_selection = st.radio("Selecione o gráfico:", ("Gráfico 1", "Gráfico 2", "Gráfico 3", "Gráfico 4", "Gráfico 5"))
 
 # Renderiza o gráfico selecionado com base na seleção do usuário
 if chart_selection == "Gráfico 1":
@@ -65,6 +65,53 @@ elif chart_selection == "Gráfico 3":
         b, key="echarts"
     )  # Add key argument to not remount component at every Streamlit run
     st.button("Randomize data")
+elif char_selection == "Gráfico 4":
+    options = {
+        "title": {"text": "Coordenações"},
+        "tooltip": {"trigger": "axis"},
+        "legend": {"data": ["CGPAT I", "CGPAT II", "CGPAT III", "CGPAT IV", "DIRPA"]},
+        "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
+        "toolbox": {"feature": {"saveAsImage": {}}},
+        "xAxis": {
+            "type": "category",
+            "boundaryGap": False,
+            "data": ["2015", "2016", "2017", "2018", "2019", "2020", "2021"],
+        },
+        "yAxis": {"type": "value"},
+        "series": [
+            {
+                "name": "CGPAT I",
+                "type": "line",
+                "stack": "st",
+                "data": [120, 132, 101, 134, 90, 230, 210],
+            },
+            {
+                "name": "CGPAT II",
+                "type": "line",
+                "stack": "st",
+                "data": [220, 182, 191, 234, 290, 330, 310],
+            },
+            {
+                "name": "CGPAT III",
+                "type": "line",
+                "stack": "st",
+                "data": [150, 232, 201, 154, 190, 330, 410],
+            },
+            {
+                "name": "CGPAT IV",
+                "type": "line",
+                "stack": "st",
+                "data": [320, 332, 301, 334, 390, 330, 320],
+            },
+            {
+                "name": "DIRPA",
+                "type": "line",
+                "stack": "st",
+                "data": [820, 932, 901, 934, 1290, 1330, 1320],
+            },
+        ],
+    }
+    st_echarts(options=options, height="400px")
 else:
     url = "http://www.cientistaspatentes.com.br/apiphp/patents/query/?q={%22mysql_query%22:%22divisao,count(*)%20FROM%20arquivados%20where%20despacho=%2715.23%27%20and%20year(data)%3E=2000%20group%20by%20divisao%20order%20by%20count(*)%20desc%22}"
     # Definindo cabeçalhos para a requisição
