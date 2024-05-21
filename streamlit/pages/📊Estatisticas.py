d4e4e1f8ad6a2a069c2f9f98de8eefbbfd2e7b04
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-05-21 20:08:46 -0300

    Update 📊Estatisticas.py

--- streamlit/pages/📊Estatisticas.py
+++ streamlit/pages/📊Estatisticas.py
@@ -41,7 +41,7 @@ chart_selection = st.radio("Selecione o gráfico:", ("Gráfico 1", "Gráfico 2",
 # Renderiza o gráfico selecionado com base na seleção do usuário
 if chart_selection == "Gráfico 1":
     render_chart(option1)
-else if chart_selection == "Gráfico 2":
+elif chart_selection == "Gráfico 2":
     render_chart(option2)
 else:
     url = "http://www.cientistaspatentes.com.br/apiphp/patents/query/?q={%22mysql_query%22:%22divisao,count(*)%20FROM%20arquivados%20where%20despacho=%2715.23%27%20and%20year(data)%3E=2000%20group%20by%20divisao%20order%20by%20count(*)%20desc%22}"
