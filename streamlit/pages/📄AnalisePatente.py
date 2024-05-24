abbac4ecd955145294c6acedc1757418963aab66
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-05-24 18:37:21 -0300

    Update 📄AnalisePatente.py

--- streamlit/pages/📄AnalisePatente.py
+++ streamlit/pages/📄AnalisePatente.py
@@ -8,6 +8,7 @@ from PyPDF2 import PdfReader
 from pathlib import Path
 import hashlib
 
+# https://share.streamlit.io/
 load_dotenv()
 # Obtém a chave da API da variável de ambiente
 # no streamlit https://share.streamlit.io/ escolha o app / Settings / Secrets e guarde a chave API do Google
@@ -39,17 +40,14 @@ st.write("Envie o pedido de patente.")
 
 # Upload do currículo
 st.write("Por favor, faça o upload do pedido em formato PDF")
-cv = st.file_uploader("Upload do pedido:", type=['pdf'])
-
-# Botões de ação
-if cv is not None:
+pedido = st.file_uploader("Upload do pedido:", type=['pdf'])
+if pedido is not None:
     with st.spinner('Carregando pedido...'):
-        text = text_from_pdf(cv)
+        text = text_from_pdf(pedido)
     st.success('Pedido carregado com sucesso!')
     
     initial_message = f"Olá Sophia, faça o resumo do pedido {text}."
-    button = st.button('Faça resumo do pedido')
-    if button:
+    if st.button('Faça resumo do pedido'):
         with st.spinner("Processando..."):
             ai_query = model.generate_content(initial_message)
             st.markdown(ai_query.text)
@@ -57,22 +55,20 @@ if cv is not None:
     st.write("Envie a anterioridade.")
     st.write("Por favor, faça o upload da anterioridade em formato PDF")
     
-    cv = st.file_uploader("Upload da anterioridade:", type=['pdf'])
-    if cv is not None:
+    prior_art = st.file_uploader("Upload da anterioridade:", type=['pdf'])
+    if prior_art is not None:
         with st.spinner('Carregando anterioridade...'):
-            text_anterioridade = text_from_pdf(cv)
+            text_anterioridade = text_from_pdf(prior_art)
         st.success('Anterioridade carregada com sucesso!')
 
         initial_message = f"Olá Sophia, faça o resumo da anterioridade e traduza para o português {text_anterioridade}."
-        button = st.button('Faça resumo da anterioridade')
-        if button:
+        if st.button('Faça resumo da anterioridade'):
             with st.spinner("Processando..."):
                 ai_query_anterioridade = model.generate_content(initial_message)
                 st.markdown(ai_query_anterioridade.text)
 
-        initial_message = f"Olá Sophia, aponte as diferenças do pedido com a anterioridade."
-        button = st.button('Análise dos documentos:')
-        if button:
+        initial_message = f"Olá Sophia, aponte as diferenças do primeiro PDF carregado com o segundo PDF carregado."
+        if st.button('Faça análise dos documentos'):
             with st.spinner("Processando..."):
                 ai_query_analise = model.generate_content(initial_message)
                 st.markdown(ai_query_analise.text)
