42ac4eb1f84f9eeba12d762094459e5b0fab0396
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-05-24 18:46:59 -0300

    oi

--- streamlit/pages/📄AnalisePatente.py
+++ streamlit/pages/📄AnalisePatente.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 import time
 from PyPDF2 import PdfReader
+import pdfplumber
 
 from pathlib import Path
 import hashlib
@@ -27,13 +28,20 @@ model = genai.GenerativeModel(
     system_instruction=system_instruction
 )
 
-def text_from_pdf(pdf):
+def text_from_pdf_old(pdf):
     text = ""
     pdf_reader = PdfReader(pdf)
     for page in pdf_reader.pages:
         text += page.extract_text()
     return text
 
+def text_from_pdf(pdf):
+    text = ""
+    with pdfplumber.open(pdf) as pdf_reader:
+        for page in pdf_reader.pages:
+            text += page.extract_text() or ""
+    return text
+    
 # Título da página
 st.title('AnalisePatente 📄')
 st.write("Envie o pedido de patente.")
