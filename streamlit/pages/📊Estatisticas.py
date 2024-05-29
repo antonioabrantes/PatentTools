25a6de5bdccf42e995d0a073840cd1fb269fe9ff
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-05-29 17:04:57 -0300

    Update 📊Estatisticas.py

--- streamlit/pages/📊Estatisticas.py
+++ streamlit/pages/📊Estatisticas.py
@@ -52,7 +52,7 @@ if chart_selection == "Gráfico 1":
     my_sql = "divisao, count(*) FROM arquivados where despacho='15.23' and year(data)>=2000 group by divisao order by count(*) desc"
     url = "http://www.cientistaspatentes.com.br/apiphp/patents/query/?q={mysql_query:" + '"' + my_sql + '}"'
 
-    url = "http://www.cientistaspatentes.com.br/apiphp/patents/query/?q={mysql_query:divisao,count(*) FROM%20arquivados%20where%20despacho=%2715.23%27%20and%20year(data)%3E=2000%20group%20by%20divisao%20order%20by%20count(*)%20desc%22}"
+    url = "http://www.cientistaspatentes.com.br/apiphp/patents/query/?q={%22mysql_query%22:%22divisao,count(*) FROM arquivados where despacho='15.23' and year(data)>=2000 group by divisao order by count(*) desc%22}"
 
     # Definindo cabeçalhos para a requisição
     headers = {
