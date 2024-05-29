bc9795f235cacf13672f14c3554337d61e982462
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-05-29 16:46:45 -0300

    Update 📊Estatisticas.py

--- streamlit/pages/📊Estatisticas.py
+++ streamlit/pages/📊Estatisticas.py
@@ -45,8 +45,69 @@ option2 = {
 chart_selection = st.radio("Selecione o gráfico:", ("Gráfico 1", "Gráfico 2", "Gráfico 3", "Gráfico 4", "Gráfico 5"))
 
 # Renderiza o gráfico selecionado com base na seleção do usuário
+#if chart_selection == "Gráfico 1":
+#    render_chart(option1)
+
 if chart_selection == "Gráfico 1":
-    render_chart(option1)
+    url = "http://www.cientistaspatentes.com.br/apiphp/patents/query/?q={mysql_query:"divisao, count(*) FROM arquivados where despacho=%'15.23' and year(data)>=2000 group by divisao order by count(*) desc}"
+    # Definindo cabeçalhos para a requisição
+    headers = {
+        "Accept": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+
+
+    try:
+        # Requisição para obter os dados JSON
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Verificar se a requisição foi bem-sucedida
+
+        # Tentar decodificar o JSON
+        data = response.json()
+
+        # Carregar os dados JSON em um DataFrame
+        df = pd.DataFrame(data['patents'])
+        df['divisao'] = df['divisao'].fillna('Unknown')
+
+        # Verificar e converter a coluna 'count' para inteiro
+        df['count'] = pd.to_numeric(df['count'], errors='coerce')
+
+        # Mostrar o DataFrame
+        st.write("Valores", df)
+
+        # Exibir o gráfico de linhas
+        # st.line_chart(df.set_index('divisao')['count'])
+        
+        fig, ax = plt.subplots()
+        ax.plot(df['divisao'], df['count'], marker='o')
+
+        # Adicionar linhas verticais
+        for i, label in enumerate(df['divisao']):
+            ax.axvline(x=i, color='gray', linestyle='--', linewidth=0.5)
+
+        # Adicionar linhas horizontais
+        for count in df['count']:
+            ax.axhline(y=count, color='gray', linestyle='--', linewidth=0.5)
+            
+        # Adicionar rótulos e título
+        ax.set_xlabel('Divisão')
+        ax.set_ylabel('Count')
+        ax.set_title('Incidência por Divisão Técnica')
+        ax.set_xticks(range(len(df['divisao'])))
+        ax.set_xticklabels(df['divisao'], rotation=90)
+
+        # Mostrar o gráfico no Streamlit
+        st.pyplot(fig)
+
+    except requests.exceptions.HTTPError as http_err:
+        st.error(f"HTTP error occurred: {http_err}")
+    except requests.exceptions.RequestException as req_err:
+        st.error(f"Error occurred during request: {req_err}")
+    except ValueError as json_err:
+        st.error(f"JSON decode error: {json_err}")
+    except Exception as err:
+        st.error(f"An unexpected error occurred: {err}")
+        
 elif chart_selection == "Gráfico 2":
     render_chart(option2)
 elif chart_selection == "Gráfico 3":
