37c0b23aaababdb01390d671526e3a922b5c9825
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-06-03 19:20:17 -0300

    aa

--- streamlit/pages/❓FAQ.py
+++ streamlit/pages/❓FAQ.py
@@ -9,10 +9,27 @@ import sys
 import time
 from jobs_details import jobs_details as data
 from dotenv import load_dotenv
+import ast
 
 # Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
 
 st.title('FAQ ❓')
-st.write("Envie o pedido de patente.")
-st.write(f"perguntas = {data}")
+st.write("Perguntas mais frequentes.")
+# st.write(f"perguntas = {data}")
+
+# Converte a string para uma lista de dicionários
+data_list = ast.literal_eval(data)
+
+# Função para imprimir os campos de forma formatada
+def print_data(data):
+    for item in data:
+        st.write(f"\033[1mTema:\033[0m {item['Tema']}")
+        st.write(f"Pergunta: {item['Pergunta']}")
+        st.write(f"Resposta: {item['Resposta']}")
+        st.write(f"Modelos: {item['Modelos']}")
+        st.write(f"Despacho: {item['Despacho']}")
+        st.write("\n\n")
+
+# Executa a função para imprimir os dados
+print_data(data_list)
