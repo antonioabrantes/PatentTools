e6328d55c82be1fc949a590ca8cd06e5bf56b359
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-06-27 14:02:00 -0300

    Update jobs_details.py

--- streamlit/jobs_details.py
+++ streamlit/jobs_details.py
@@ -1,17 +1,11 @@
 jobs_details = [
     {
-        "Tema": "Instalação",
-        "Pergunta": "Como instalo o software?",
-        "Resposta": "Para instalar o software, siga as instruções no manual.",
-        "Modelos": ["Modelo A", "Modelo B"],
-        "Despacho": "Sim"
-    },
-    {
-        "Tema": "Configuração",
-        "Pergunta": "Como configuro o software?",
-        "Resposta": "Para configurar o software, acesse o menu de configurações.",
-        "Modelos": ["Modelo C", "Modelo D"],
-        "Despacho": "Não"
+        "id": 1,
+        "Tema": "Vício de procedimento",
+        "Pergunta": "No parecer de indeferimento não houve o tratamento de todas as petições apresentadas relacionadas diretamente ao mérito da decisão impugnada, por exemplo de existe uma petição subsídio ao exame que foi desconsiderada",
+        "Resposta": "O examinador de recurso deve se certificar de que todas as petições pertinentes ao exame de mérito foram consideradas no parecer de indeferimento. O examinador recursal deve verificar se o quadro reivindicatório analisado é o correto e se foi desconsiderada alguma das petições apresentadas relacionadas diretamente ao mérito da decisão impugnada, por exemplo uma petição de subsídio ao exame que foi desconsiderada. Tendo em vista que existe um Vício de procedimento que impede a resolução do mérito do recurso, opina-se pela anulação da decisão proferida, com consequente retorno dos autos à primeira instância, para a continuação do exame. A análise do julgamento de indeferimento está prejudicada em razão do Vício de procedimento. Fica igualmente prejudicada a análise sobre a possibilidade de modificações no pedido.",
+        "Modelos": [1],
+        "Despacho": "Despacho 100.2. Recurso conhecido e provido para correção do Vício de procedimento. Anulado o indeferimento para retorno dos autos à Primeira Instância."
     }
 ]
 
