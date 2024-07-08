6a4d4586998e47d87ff32b230451f55a12f100f1
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-07-08 18:43:09 -0300

    Update ❓FAQ.py

--- streamlit/pages/❓FAQ.py
+++ streamlit/pages/❓FAQ.py
@@ -23,8 +23,13 @@ try:
     #data_list = ast.literal_eval(data)
     data_list = data
 
-    st.markdown("![Screenshot of a comment](https://myoctocat.com/assets/images/base-octocat.svg)")
-   
+    # st.markdown("![Screenshot of a comment](https://myoctocat.com/assets/images/base-octocat.svg)")
+    st.markdown(
+    """
+    <img src="https://myoctocat.com/assets/images/base-octocat.svg" width="300"/>
+    """, 
+    unsafe_allow_html=True
+    )
     
     # Função para imprimir os campos de forma formatada
     def print_data(data):
