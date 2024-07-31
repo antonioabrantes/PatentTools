9f8b3b1b4b2dc7fcceb034dfc023fb9843b75268
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-07-30 22:52:04 -0300

    Update 💬BatePapoChatGPT.py

--- streamlit/pages/💬BatePapoChatGPT.py
+++ streamlit/pages/💬BatePapoChatGPT.py
@@ -42,9 +42,9 @@ client = ElevenLabs(
 )
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
-os.environ["OPENAI_API_KEY"]=openai_api_key
+#os.environ["OPENAI_API_KEY"]=openai_api_key
 gemini_api_key = os.getenv("GEMINI_API_KEY")
-os.environ["GOOGLE_API_KEY"] = gemini_api_key
+#os.environ["GOOGLE_API_KEY"] = gemini_api_key
 anthropic_api_key = os.getenv("ANTHROPIC_API_KEY")
 # os.environ["ANTHROPIC_API_KEY"]=anthropic_api_key
 # os.environ["ANTHROPIC_API_KEY"] = getpass.getpass("Enter your Anthropic API key: ")
