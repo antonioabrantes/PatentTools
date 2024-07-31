e84c1a3a896390c6ae4e23f51fbd70c85bdb7440
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-07-30 23:53:15 -0300

    Update 💬BatePapoChatGPT.py

--- streamlit/pages/💬BatePapoChatGPT.py
+++ streamlit/pages/💬BatePapoChatGPT.py
@@ -24,6 +24,7 @@ from playsound import playsound
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
+from langchain_core.runnables import RunnablePassthrough
 
 from transformers import GPT2TokenizerFast
 from langchain.document_loaders import PyPDFLoader
