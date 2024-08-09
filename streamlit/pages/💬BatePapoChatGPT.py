4acc94965da996eee1c047da4fe08ff2b2aad961
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-08-09 17:33:47 -0300

    Update 💬BatePapoChatGPT.py

--- streamlit/pages/💬BatePapoChatGPT.py
+++ streamlit/pages/💬BatePapoChatGPT.py
@@ -263,6 +263,7 @@ if user_query is not None and user_query != '':
         numero = utils.extrair_numero_pedido(user_query)
         # numero = '102023024151' PI9809089 11201300558 
         numero= '11201300558'
+        numero= '112013005558'
         # https://www.cientistaspatentes.com.br/apiphp/patents/query/?q="mysql_query":"* FROM arquivados where numero='112013005558' and anulado=0 order by data desc"
         
         query = '"' + "mysql_query" + '"' ":" + '"' + f" * FROM arquivados where numero='{numero}' and anulado=0 order by data desc" + '"'
