631457037f73e7586a4fed5fab53ae224ae61e61
Antonio Carlos Souza de Abrantes <otimistarj@gmail.com>
2024-10-13 19:14:47 -0300

    Update 📊Estatisticas.py

--- streamlit/pages/📊Estatisticas.py
+++ streamlit/pages/📊Estatisticas.py
@@ -338,14 +338,14 @@ elif chart_selection == "Gráfico 5":
             # Mostrar o gráfico no Streamlit
             st.pyplot(fig)
             
-    except requests.exceptions.HTTPError as http_err:
-        st.error(f"HTTP error occurred: {http_err}")
-    except requests.exceptions.RequestException as req_err:
-        st.error(f"Error occurred during request: {req_err}")
-    except ValueError as json_err:
-        st.error(f"JSON decode error: {json_err}")
-    except Exception as err:
-        st.error(f"An unexpected error occurred: {err}")
+        except requests.exceptions.HTTPError as http_err:
+            st.error(f"HTTP error occurred: {http_err}")
+        except requests.exceptions.RequestException as req_err:
+            st.error(f"Error occurred during request: {req_err}")
+        except ValueError as json_err:
+            st.error(f"JSON decode error: {json_err}")
+        except Exception as err:
+            st.error(f"An unexpected error occurred: {err}")
 else:
     url = "http://www.cientistaspatentes.com.br/apiphp/patents/query/?q={%22mysql_query%22:%22divisao,count(*)%20FROM%20arquivados%20where%20despacho=%2715.23%27%20and%20year(data)%3E=2000%20group%20by%20divisao%20order%20by%20count(*)%20desc%22}"
     # Definindo cabeçalhos para a requisição
